4fc0d8a058a2d98491a97cfcccca92d9939ffead
illa4257 <48959534+illa4257@users.noreply.github.com>
2024-01-03 09:17:57 +0200

    New constructor for WebImage.java

--- src/Utils/web/WebImage.java
+++ src/Utils/web/WebImage.java
@@ -7,15 +7,37 @@ import java.io.ByteArrayOutputStream;
 import java.net.URI;
 
 public class WebImage extends LoadingImage {
-    public final URI uri;
+    public final sURL url;
 
     public WebImage(final WebClient client, final URI uri) {
-        this.uri = uri;
+        this.url = new sURL(uri);
         if (uri.getScheme().equals("https") || uri.getScheme().equals("http")) {
             new Thread(() -> {
                 try {
                     final ByteArrayOutputStream os = new ByteArrayOutputStream();
                     final WebResponse r = client.open("GET", uri, os, true);
+                    r.auto();
+                    if (r.responseCode == WebResponse.OK)
+                        setImage(UI.image(os.toByteArray()));
+                } catch (final Exception ex) {
+                    ex.printStackTrace();
+                }
+            }).start();
+            return;
+        }
+        setImage(null);
+    }
+
+    /**
+     * @since AdvancedAPICore 0.2.3
+     */
+    public WebImage(final WebClient client, final sURL url) {
+        this.url = url;
+        if (url.scheme.equals("https") || url.scheme.equals("http")) {
+            new Thread(() -> {
+                try {
+                    final ByteArrayOutputStream os = new ByteArrayOutputStream();
+                    final WebResponse r = client.open("GET", url, os, true);
                     r.auto();
                     if (r.responseCode == WebResponse.OK)
                         setImage(UI.image(os.toByteArray()));
